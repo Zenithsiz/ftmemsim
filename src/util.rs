cf52bf745d2f3b1120bbb1482cb2f3b06f62564e
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-05-17 19:15:52 +0100

    Added `util::DisplayWrapper` helper.

--- src/util.rs
+++ src/util.rs
@@ -7,7 +7,7 @@ mod duration;
 pub use duration::FemtoDuration;
 
 // Imports
-use std::io;
+use std::{cell::RefCell, fmt, io};
 
 /// Extension trait for `R: io::Read` types to read a byte array
 #[extend::ext(name = ReadByteArray)]
@@ -19,5 +19,24 @@ pub impl<R: io::Read> R {
 		let mut array = [0u8; N];
 		self.read_exact(&mut array)?;
 		Ok(array)
+	}
+}
+
+/// [`fmt::Display`] helper to display using a `FnMut(&mut fmt::Formatter)`
+pub struct DisplayWrapper<F: FnMut(&mut fmt::Formatter) -> fmt::Result>(RefCell<F>);
+
+impl<F: FnMut(&mut fmt::Formatter) -> fmt::Result> DisplayWrapper<F> {
+	/// Creates a new display wrapper
+	#[must_use]
+	pub const fn new(func: F) -> Self {
+		Self(RefCell::new(func))
+	}
+}
+
+
+impl<F: FnMut(&mut fmt::Formatter) -> fmt::Result> fmt::Display for DisplayWrapper<F> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		// Note: `f` cannot be re-entrant, so this cannot fail
+		self.0.borrow_mut()(f)
 	}
 }
